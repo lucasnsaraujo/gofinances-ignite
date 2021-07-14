70e96058dd2fe5a5e6982d0d9d7a747218c6a263
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2021-07-13 23:20:04 -0300

    implemented styled components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
 import { Dashboard } from './src/screens/Dashboard';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/themes/theme'
+
+import AppLoading from 'expo-app-loading'
+
+import { 
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  }); // handle if fonts were loaded correctly, if not, splash screen stays
+
+  if (!fontsLoaded) {
+    <AppLoading/>
+  }
+
   return (
-  <Dashboard/>
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Container, Header } from './styles'
+
 
 export function Dashboard() {
   return (
-    <View style={styles.container}>
-      <Text>Dashboard</Text>
-    </View>
+    <Container>
+      <Header>
+      </Header>
+    </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
