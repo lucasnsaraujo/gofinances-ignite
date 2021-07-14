7deeef7f200037c007eb2c1aa6ebcbf22174b2c3
nasci <lucasnsaraujo0310@gmail.com>
2021-07-14 09:02:14 -0300

    dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import { Container, Header } from './styles'
+import { 
+  Container, 
+  Header,
+  UserInfo,
+  Photo, 
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper
+} from './styles'
 
 
 export function Dashboard() {
   return (
     <Container>
       <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri : 'https://github.com/lucasnsaraujo.png'}}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Lucas</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
       </Header>
+
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native'
-import {RFPercentage} from 'react-native-responsive-fontsize'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 
 export const Container = styled.View`
   flex: 1;
@@ -10,5 +10,39 @@ export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
   background-color: ${({theme}) => theme.colors.primary};
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Photo = styled.Image`
+  width: ${RFValue(55)}px;
+  height: ${RFValue(55)}px;
+  border-radius: 10px;
+  
+`
+
+export const User = styled.View`
+  margin-left: 17px ;
+`
+
+export const UserGreeting = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-family: ${({theme}) => theme.fonts.regular};
+`
+
+export const UserName = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-family: ${({theme}) => theme.fonts.bold};
 
 `
